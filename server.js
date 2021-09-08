dc655c8466fd95eebd270dacc37dac3b3e30bd53
Maßmann, Kevin <kevin.massmann@adesso.de>
2021-09-08 13:54:38 +0200

    compression for heroku build

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
+const compression = require('compression')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -8,10 +10,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
